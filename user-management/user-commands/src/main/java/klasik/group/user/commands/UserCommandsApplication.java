2910197bb45ce7cff1b527dd8275049f9a664df7
Stanislav Kasika <stanislav.kasika@gmail.com>
2021-01-21 19:34:56 +0100

    add new modules

---
+++ user-management/user-commands/src/main/java/klasik/group/user/commands/UserCommandsApplication.java
@@ -0,0 +1,13 @@
+package klasik.group.user.commands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCommandsApplication.class, args);
+    }
+
+}
---
+++ user-management/user-oauth2/src/test/java/klasik/group/user/oauth2/UserOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package klasik.group.user.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
